79dee7116d4b4ce516804f77e502479ff51bcf17
Fabien Chouteau <chouteau@adacore.com>
2015-12-09 22:00:12 +0100

    Run RNG a few time at start-up

--- src/main.adb
+++ src/main.adb
@@ -8,10 +8,17 @@ with STM32F4.RNG.Polling;
 pragma Warnings (Off);
 with Last_Chance_Handler;
 pragma Warnings (On);
+with Interfaces;
 
 procedure Main is
+   Unused : Interfaces.Unsigned_32;
 begin
    STM32F4.RNG.Polling.Initialize_RNG;
+
+   for Index in 1 .. 10 loop
+      Unused := STM32F4.RNG.Polling.Random;
+   end loop;
+
    Candy_Dispenser.Initialize;
    GUI.Initialize;
    GUI.Start;
